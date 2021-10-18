7d8ffa8d0be4143010848c0004dd407508ef5d37
Marvin <marvinnrn@gmail.com>
2021-10-18 20:55:46 +0200

    bug fix

--- mrn-project/src/components/projectsHover/ReserviaHover.js
+++ mrn-project/src/components/projectsHover/ReserviaHover.js
@@ -4,7 +4,7 @@ const ReserviaHover = () => {
     return (
         <div className="cardsReserviaHover">
             <p className="cardsText">Projet n°2 du parcours développeur web OpenClassrooms.
-                Intégration d'un site de réservation d'hôtel a Marseille, tout en respectant les maquettes (desktop et mobile) et les contraintes imposées par le client</p>
+                Intégration d'un site de réservation d'hôtel a Marseille, tout en respectant les maquettes et les contraintes imposées par le client</p>
             <p className="reserviaAlign"><a className="cardsLink" href='https://marvinrn.github.io/MarvinRodriguesNovo_2_07032021/' target="_blank" rel="noopener noreferrer">Voir Projet</a></p>
         </div>
     );
