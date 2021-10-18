120f51017873895861af4195f9fc71e5436f2a61
Marvin <marvinnrn@gmail.com>
2021-10-18 21:02:46 +0200

    correction de faute orthographe

--- mrn-project/src/components/Introduction.js
+++ mrn-project/src/components/Introduction.js
@@ -94,8 +94,8 @@ const Introduction = () => {
                 <div className="selfIntro" id="introduction">
                     <h1 className="selfTitle">Qui suis-je?</h1>
                     <br />
-                    <p> Salut, je m'appel Marvin Rodrigues Novo et je suis un jeune développeur web front-end en recherche d'une alternance. Je débute le développement web front-end depuis seulement quelques mois et je suis déja passionné par ce milieu, c'est donc pour cette raison que j'ai décidé de faire du développement web mon métier de rêve.<br />
-                        Sur ce site vous retrouverez les différents projets que j'ai pu réaliser ainsi que mes futurs projets à venir.
+                    <p> Salut, je m'appelle Marvin Rodrigues Novo et je suis un jeune développeur web front-end en recherche d'une alternance. Je débute le développement web front-end depuis seulement quelques mois et je suis déjà passionné par ce milieu, c'est donc pour cette raison que j'ai décidé de faire du développement web mon métier de rêve.<br />
+                        Sur ce site vous retrouverez les différents projets que j'ai pu réaliser, ainsi que mes futurs projets à venir.
                     </p>
                 </div>
             </section>
